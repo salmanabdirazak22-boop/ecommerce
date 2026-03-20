bb6bf391f77640565df77b932c32b09b1096547c
anas abdirazak abdillahi <salmanabdirazak22@gmail.com>
2026-03-20 07:21:58 +0300

    fix: switch to bcryptjs and upgrade node engine

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { prisma } from "@/lib/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
